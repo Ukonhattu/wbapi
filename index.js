c72022fb61819bd2577e4c61abf0b73a8a10c868
Daniel Koch <daniel.koch@helsinki.fi>
2019-02-17 02:38:32 +0200

    API for country&year querys (co2 and population)

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 const api = require('./wbapi')
+const cors = require('cors')
+app.use(cors())
 
 app.get('/co2', (req, res) => {
     res.send(api.co2json())
 })
 
-app.get('co2/country/:name', (req, res) => {
+app.get('/co2/country/:name', (req, res) => {
     const name = req.params.name
-    res.send(api.co2country(name))
+    res.send(api.co2Country(name))
+})
+
+app.get('/co2/country/:name/:year', (req, res) => {
+    const {name, year} = req.params
+    res.send(api.co2CountryYear(name, year))
 })
 
 app.get('/pop', (req, res) => {
@@ -17,7 +24,12 @@ app.get('/pop', (req, res) => {
 
 app.get('/pop/country/:name', (req, res) => {
     const name = req.params.name
-    res.send(api.popcountry(name))
+    res.send(api.popCountry(name))
+})
+
+app.get('/pop/country/:name/:year', (req, res) => {
+    const {name, year} = req.params
+    res.send(api.popCountryYear(name, year))
 })
 const PORT = 3001
 app.listen(PORT, () => {
--- wbapi.js
+++ wbapi.js
@@ -15,12 +15,43 @@ csv().fromFile(populationFilePath).then((jsonObj) => {
 
 exports.co2json = () => co2
 
-exports.co2country = (name) => {
+exports.co2Country = (name) => {
     return co2.filter(p => p['Data Source'] === name)
+}
+
+exports.co2CountryYear = (name, year) => {
+    return co2.filter(p => p['Data Source'] === name)[0][getYearField(year, co2)]
 }
 
 exports.populationjson = () => population
 
-exports.popcountry = (name) => {
+exports.popCountry = (name) => {
     return population.filter(p => p['Data Source'] === name)
 }
+
+exports.popCountryYear = (name, year) => {
+    return population.filter(p => p['Data Source'] === name)[0][getYearField(year, population)]
+}
+
+getYearField = (year, obj) => {
+    for (let key in obj[1]) {
+        let attrName = key;
+        let attrValue = obj[1][key]
+        if (attrValue === year.toString()) return attrName
+    }
+    return "undefined year"
+}
+
+// beautify = (arr) => {
+//     const start = 1960
+//     let str = JSON.stringify(arr)
+//     for (i = 5; i < 65; i++) {
+//         let rep = "field".concat(i)
+//         console.log(rep)
+//         let regex = new RegExp(rep, "g")
+//         let year = start + (i-5) 
+//         str = str.replace(regex, year.toString())
+//     }
+//     arr = JSON.parse(str)
+//     return arr
+// }
