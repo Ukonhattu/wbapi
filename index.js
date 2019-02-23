069d07cdd71d5100581c5b8a2b35f42b8e4876a6
Daniel Koch <daniel.koch@helsinki.fi>
2019-02-23 18:47:03 +0200

    [FIX] fixed all get urls

--- index.js
+++ index.js
@@ -5,49 +5,49 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 
-app.get('api/co2', (req, res) => {
+app.get('/api/co2', (req, res) => {
     res.send(api.co2json())
 })
 
-app.get('api/co2/country/:name', (req, res) => {
+app.get('/api/co2/country/:name', (req, res) => {
     const name = req.params.name
     res.send(api.co2Country(name))
 })
 
-app.get('api/co2/country/:name/:year', (req, res) => {
+app.get('/api/co2/country/:name/:year', (req, res) => {
     const {name, year} = req.params
     res.send(api.co2CountryYear(name, year))
 })
 
-app.get('api/co2/country/:name/:start/:end', (req, res) => {
+app.get('/api/co2/country/:name/:start/:end', (req, res) => {
     const name = req.params.name
     const start = Number(req.params.start)
     const end = Number(req.params.end)
     res.send(api.co2Range(name, start, end))
 })
 
-app.get('api/pop', (req, res) => {
+app.get('/api/pop', (req, res) => {
     res.send(api.populationjson())
 })
 
-app.get('api/pop/country/:name', (req, res) => {
+app.get('/api/pop/country/:name', (req, res) => {
     const name = req.params.name
     res.send(api.popCountry(name))
 })
 
-app.get('api/pop/country/:name/:year', (req, res) => {
+app.get('/api/pop/country/:name/:year', (req, res) => {
     const {name, year} = req.params
     res.send(api.popCountryYear(name, year))
 })
 
-app.get('api/pop/country/:name/:start/:end', (req, res) => {
+app.get('/api/pop/country/:name/:start/:end', (req, res) => {
     const name = req.params.name
     const start = Number(req.params.start)
     const end = Number(req.params.end)
     res.send(api.popRange(name, start, end))
 })
 
-app.get('api/co2/country/:name/:start/:end/:percapita', (req, res) => {
+app.get('/api/co2/country/:name/:start/:end/:percapita', (req, res) => {
     const name = req.params.name
     const start = Number(req.params.start)
     const end = Number(req.params.end)
