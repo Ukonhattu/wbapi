9348ea484a6d9786f8cec6e4cc7404053edbc88a
Daniel Koch <daniel.koch@helsinki.fi>
2019-02-23 18:43:13 +0200

    static front for production

---
+++ build/precache-manifest.f4e62bf10d14af9d93f73e463600385a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ed51eaa3d85163c9dd",
+    "url": "/static/css/main.91f4451a.chunk.css"
+  },
+  {
+    "revision": "24ed51eaa3d85163c9dd",
+    "url": "/static/js/main.24ed51ea.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c1a7c44590f71dbbc6b0",
+    "url": "/static/css/2.9388646f.chunk.css"
+  },
+  {
+    "revision": "c1a7c44590f71dbbc6b0",
+    "url": "/static/js/2.c1a7c445.chunk.js"
+  },
+  {
+    "revision": "3327ccb662e65499a24530614ef9e612",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -3,50 +3,51 @@ const app = express()
 const api = require('./wbapi')
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('build'))
 
-app.get('/co2', (req, res) => {
+app.get('api/co2', (req, res) => {
     res.send(api.co2json())
 })
 
-app.get('/co2/country/:name', (req, res) => {
+app.get('api/co2/country/:name', (req, res) => {
     const name = req.params.name
     res.send(api.co2Country(name))
 })
 
-app.get('/co2/country/:name/:year', (req, res) => {
+app.get('api/co2/country/:name/:year', (req, res) => {
     const {name, year} = req.params
     res.send(api.co2CountryYear(name, year))
 })
 
-app.get('/co2/country/:name/:start/:end', (req, res) => {
+app.get('api/co2/country/:name/:start/:end', (req, res) => {
     const name = req.params.name
     const start = Number(req.params.start)
     const end = Number(req.params.end)
     res.send(api.co2Range(name, start, end))
 })
 
-app.get('/pop', (req, res) => {
+app.get('api/pop', (req, res) => {
     res.send(api.populationjson())
 })
 
-app.get('/pop/country/:name', (req, res) => {
+app.get('api/pop/country/:name', (req, res) => {
     const name = req.params.name
     res.send(api.popCountry(name))
 })
 
-app.get('/pop/country/:name/:year', (req, res) => {
+app.get('api/pop/country/:name/:year', (req, res) => {
     const {name, year} = req.params
     res.send(api.popCountryYear(name, year))
 })
 
-app.get('/pop/country/:name/:start/:end', (req, res) => {
+app.get('api/pop/country/:name/:start/:end', (req, res) => {
     const name = req.params.name
     const start = Number(req.params.start)
     const end = Number(req.params.end)
     res.send(api.popRange(name, start, end))
 })
 
-app.get('/co2/country/:name/:start/:end/:percapita', (req, res) => {
+app.get('api/co2/country/:name/:start/:end/:percapita', (req, res) => {
     const name = req.params.name
     const start = Number(req.params.start)
     const end = Number(req.params.end)
